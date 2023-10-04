7cbfaa4fbbdee7b55d37101c7b43ff2dfedc5edc
Ozodbekochilov <ozodbekochilov449@gmail.com>
2023-10-04 17:48:20 +0500

    first commit

---
+++ resources/views/table.blade.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+            table
+{
+    width:100%;
+    height:500px;
+    text-align:center;
+    border:1px solid black;
+    border-collapse: collapse;
+}
+
+th,td {
+    border:1px solid black;
+}
+
+tr:hover
+{
+    background-color:grey;
+}
+    </style>
+</head>
+<body>
+  
+<table class="table table-hover bg-white">
+                      <tr>
+                      <th>№</th>
+                      <th>Shaxarlar</th>
+                      <th>Yanvar</th>
+                      <th>Fevral</th>
+                      <th>Mart</th>
+                      <th>May</th>
+                      <th>Iyun</th>
+                      <th>Iyul</th>
+                      <th>Avgust</th>
+            
+                    </tr>
+                      <tr>
+                        <td>1</td>
+                        <td>Buxoro</td>
+                        <td>62 000</td>
+                        <td>15 000</td>
+                        <td>98 000</td>
+                        <td>14 000</td>
+                        <td>21 000</td>
+                        <td>89 000</td>
+                        <td>56 000</td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Toshkent</td>
+                        <td>98 000</td>
+                        <td>78 000</td>
+                        <td>15 000</td>
+                        <td>15 000</td>
+                        <td>21 000</td>
+                        <td>98 000</td>
+                        <td>15 000</td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td>Navoiy</td>
+                        <td>98 000</td>
+                        <td>21 000</td>
+                        <td>15 000</td>
+                        <td>78 000</td>
+                        <td>62 000</td>
+                        <td>15 000</td>
+                        <td>98 000</td>
+                      </tr>
+                      <tr>
+                        <td>4</td>
+                        <td>Xorazm</td>
+                        <td>78 000</td>
+                        <td>15 000</td>
+                        <td>98 000</td>
+                        <td>15 000</td>
+                        <td>21 000</td>
+                        <td>78 000</td>
+                        <td>98 000</td>
+                      </tr>
+                      <tr>
+                        <td>5</td>
+                        <td>Surxandaryo</td>
+                        <td>21 000</td>
+                        <td>62 000</td>
+                        <td>98 000</td>
+                        <td>15 000</td>
+                        <td>15 000</td>
+                        <td>78 000</td>
+                        <td>62 000</td>
+                      </tr>
+                      <tr>
+                        <td>6</td>
+                        <td>Sirdaryo</td>
+                        <td>15 000</td>
+                        <td>78 000</td>
+                        <td>15 000</td>
+                        <td>62 000</td>
+                        <td>98 000</td>
+                        <td>21 000</td>
+                        <td>98 000</td>
+                      </tr>
+                      <tr>
+                        <td>7</td>
+                        <td>Andijon</td>
+                        <td>98 000</td>
+                        <td>78 000</td>
+                        <td>21 000</td>
+                        <td>62 000</td>
+                        <td>15 000</td>
+                        <td>98 000</td>
+                        <td>15 000</td>
+                      </tr>
+                      <tr>
+                        <td>8</td>
+                        <td>Namangan</td>
+                        <td>62 000</td>
+                        <td>15 000</td>
+                        <td>98 000</td>
+                        <td>78 000</td>
+                        <td>15 000</td>
+                        <td>21 000</td>
+                        <td>78 000</td>
+                      </tr>
+                      <tr>
+                        <td>9</td>
+                        <td>Farg`ona</td>
+                        <td>78 000</td>
+                        <td>15 000</td>
+                        <td>98 000</td>
+                        <td>21 000</td>
+                        <td>62 000</td>
+                        <td>15 000</td>
+                        <td>78 000</td>
+                      </tr>
+                      <tr>
+                        <td>10</td>
+                        <td>Jizzax</td>
+                        <td>98 000</td>
+                        <td>15 000</td>
+                        <td>86 000</td>
+                        <td>97 000</td>
+                        <td>62 000</td>
+                        <td>87 000</td>
+                        <td>98 000</td>
+                      </tr>
+                      <tr>
+                        <td>11</td>
+                        <td>Qo`raqalpog`iston</td>
+                        <td>98 000</td>
+                        <td>15 000</td>
+                        <td>86 000</td>
+                        <td>97 000</td>
+                        <td>62 000</td>
+                        <td>39 000</td>
+                        <td>78 000</td>
+                      </tr>
+                      <tr>
+                        <td>12</td>
+                        <td>Samarqand</td>
+                        <td>98 000</td>
+                        <td>15 000</td>
+                        <td>86 000</td>
+                        <td>97 000</td>
+                        <td>62 000</td>
+                        <td>39 000</td>
+                        <td>78 000</td>
+                      </tr>
+                  </table>
+                  <br>
+                  <button class="btn btn-primary"><a style="color:white;" href="http://kino.loc/ozod ">Next</a></button>
+
+</body>
+</html>
+
+
+
+
+
+
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <style>
+        #iyl
+        {
+            color:white;
+        }
+
+
+        #rasm
+    {
+        width:300px;
+        border-radius:5px ;
+
+    }
+    
+    #rasm:hover
+    {
+        width:330px;
+    }
+
+    a:hover{
+        text-decoration:none;
+    }
+    </style>
+
+
+</head>
+<body>
+
+<header class="site-navbar bg-dark" role="banner">
+    
+   
+    <nav class="navbar navbar-expand-sm  " style="background-color: transparent;">
+      <h1 class="text-white" style="padding-left:50px;" >KinoClub</h1>
+      </div>
+    <ul class="navbar-nav " style="float:right; padding-left:600px;"  >
+    
+    
+      <li class="nav-item">
+    
+        
+    <button class="btn  text-white" style="border:none" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Fantastik 
+        </button>
+       
+    
+    
+    
+      </li>
+      <li class="nav-item">
+    
+        
+    <button class="btn  text-white" style="border:none" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Premyeralar 
+        </button>
+       
+</li>
+<li class="nav-item">
+    
+        
+    <button class="btn  text-white" style="border:none" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Multiklar 
+        </button>
+       
+</li>
+
+<li class="nav-item">
+    
+        
+    <button class="btn  text-white" style="border:none" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Seriallar 
+        </button>
+       
+</li>
+
+<li class="nav-item">
+    
+        
+    <button class="btn text-white " style="border:none" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Kinolar 
+        </button>
+       
+</li>
+
+<li class="nav-item">
+    
+        
+<button class="btn btn-primary " ><a href="http://kino.loc/table" id="iyl">Next</a></button>
+
+       
+</li>
+    </ul>
+    </nav>
+</header>
+
+    <br>
+        <br>
+        <div class="row">
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm1.jpg" alt="" id="rasm">
+            </div>
+        
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm2.jpg" alt="" id="rasm">
+            </div>
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm3.jpg" alt="" id="rasm">
+            </div>
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm4.jpg" alt="" id="rasm">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm5.jpg" alt="" id="rasm">
+            </div>
+        
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm6.jpg" alt="" id="rasm">
+            </div>
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm7.jpg" alt="" id="rasm">
+            </div>
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm8.jpg" alt="" id="rasm">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm9.jpg" alt="" id="rasm">
+            </div>
+        
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm10.jpg" alt="" id="rasm">
+            </div>
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm11.jpg" alt="" id="rasm">
+            </div>
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm12.jpg" alt="" id="rasm">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm13.jpg" alt="" id="rasm">
+            </div>
+        
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm14.jpg" alt="" id="rasm">
+            </div>
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm15.jpg" alt="" id="rasm">
+            </div>
+            <div class="col-sm-3" >
+                <img src="asset/rasm/rasm16.jpg" alt="" id="rasm">
+            </div>
+        </div>
+        <br>
+</body>
+</html>
